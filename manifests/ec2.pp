a09a0efab85451106ddfc2ecd2ca556185ba39c8
Chris Pisano <pisanoc@advisory.com>
2016-11-03 10:52:31 -0400

    building master instances

---
+++ manifests/ec2.pp
@@ -0,0 +1,27 @@
+class discr33t_aws::ec2 {
+
+  ec2_instance { 'discr33t-master01':
+    ensure                   => running,
+    region                   => 'us-east-1',
+    availability_zone        => 'us-east-1a',
+    subnet                   => [
+      'discr33t-privatezone01'
+    ],
+    security_groups          => [
+      'discr33t-default',
+      'discr33t-remoteaccess'
+    ],
+    image_id                 => 'ami-6d1c2007',
+    key_name                 => 'discr33t',
+    instance_type            => 't2.micro',
+    monitoring               => false,
+    ebs_optimized            => false,
+    iam_instance_profile_arn => 'arn:aws:iam::949402414914:instance-profile/Discr33tAdmin',
+    virtualization_type      => 'hvm',
+    block_devices            => [
+      {
+        'delete_on_termination' => 'true',
+        'device_name'           => '/dev/sda1'
+      }
+    ],
+  }
